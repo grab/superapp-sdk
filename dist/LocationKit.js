309e6aeb477d5165431460c679e98ecc5ada167c
Viet Hai Pham <viethai.pham@grabtaxi.com>
2019-04-23 18:06:11 +0800

    feat(kits): add copyright headers

--- dist/LocationKit.js
+++ dist/LocationKit.js
@@ -1,7 +1,8 @@
 /**
- * This class is automatically generated. DO NOT MODIFY.
- * Want to report issues/suggestions?
- * Please contact Mobile Platform at https://grab.slack.com/messages/CDN788BLY
+ * Copyright (c) Grab Taxi Holdings PTE LTD (GRAB)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 const bridgeSDK = require('@grabjs/mobile-kit-bridge-sdk');
