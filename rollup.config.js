6e3ea21a1e1e7db7a1dfe92e6ad9069d2d2f0378
Arun Sundaramurthy <arun.sundaramurthy@grabtaxi.com>
2019-04-26 17:50:53 +0800

    feat(kits): add rollup for build

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import babel from 'rollup-plugin-babel';
+import commonJs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import { uglify } from 'rollup-plugin-uglify';
+import pkg from './package.json';
+
+function createConfig({ file = 'dist/index.js' }) {
+  return {
+    input: 'src/index.js',
+    output: {
+      format: 'umd',
+      file,
+      name: 'supperappSDK',
+    },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      commonJs(),
+      resolve(),
+      uglify()
+    ]
+  };
+}
+
+export default [createConfig({})];
