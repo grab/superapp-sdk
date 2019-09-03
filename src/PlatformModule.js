99d08ab41bd1ee4493263bd63241859c84d99044
Oleksandr Prokofiev <oleksandr.prokofiev@grabtaxi.com>
2019-09-03 12:16:17 +0800

    Add PlatformKit

---
+++ src/PlatformModule.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) Grab Taxi Holdings PTE LTD (GRAB)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const bridgeSDK = require('@grabjs/mobile-kit-bridge-sdk');
+
+export class PlatformModule {
+    constructor() {
+        bridgeSDK.wrapModule(window, 'PlatformModule')
+    }
+
+    back() {
+        return window.WrappedPlatformModule.invoke('back');
+    }
+
+}
