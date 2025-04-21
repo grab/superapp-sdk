bac86c22c8e301127585c231aba76f2779d49bd0
Maxime Debersaques <maxime.debersaques@grabtaxi.com>
2025-04-21 15:23:12 +0800

    [CXTTM-0000] Lint

--- src/ContainerModule.js
+++ src/ContainerModule.js
@@ -5,44 +5,44 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const bridgeSDK = require("@grabjs/mobile-kit-bridge-sdk");
+const bridgeSDK = require('@grabjs/mobile-kit-bridge-sdk');
 
 export class ContainerModule {
   constructor() {
-    bridgeSDK.wrapModule(window, "ContainerModule");
+    bridgeSDK.wrapModule(window, 'ContainerModule');
   }
 
   setBackgroundColor(backgroundColor) {
-    return window.WrappedContainerModule.invoke("setBackgroundColor", {
+    return window.WrappedContainerModule.invoke('setBackgroundColor', {
       backgroundColor,
     });
   }
 
   setTitle(title) {
-    return window.WrappedContainerModule.invoke("setTitle", { title });
+    return window.WrappedContainerModule.invoke('setTitle', { title });
   }
 
   hideBackButton() {
-    return window.WrappedContainerModule.invoke("hideBackButton", {});
+    return window.WrappedContainerModule.invoke('hideBackButton', {});
   }
 
   showBackButton() {
-    return window.WrappedContainerModule.invoke("showBackButton", {});
+    return window.WrappedContainerModule.invoke('showBackButton', {});
   }
 
   hideRefreshButton() {
-    return window.WrappedContainerModule.invoke("hideRefreshButton", {});
+    return window.WrappedContainerModule.invoke('hideRefreshButton', {});
   }
 
   showRefreshButton() {
-    return window.WrappedContainerModule.invoke("showRefreshButton", {});
+    return window.WrappedContainerModule.invoke('showRefreshButton', {});
   }
 
   hideLoader() {
-    return window.WrappedContainerModule.invoke("hideLoader", {});
+    return window.WrappedContainerModule.invoke('hideLoader', {});
   }
 
   close() {
-    return window.WrappedContainerModule.invoke("close", {});
+    return window.WrappedContainerModule.invoke('close', {});
   }
 }
