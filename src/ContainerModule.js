1be06d5a91698f79f1f9577ccb9d0f33ce77878f
Maxime Debersaques <maxime.debersaques@grabtaxi.com>
2025-04-30 15:16:44 +0800

    [CXTTM-4008] Update logic

--- src/ContainerModule.js
+++ src/ContainerModule.js
@@ -84,83 +84,130 @@ export class ContainerModule {
 
     switch (eventDetails.eventName) {
       case AnalyticsEventName.STARTED:
-        if (
-          eventDetails.eventData !== null &&
-          eventDetails.eventData !== undefined
-        ) {
-          return 'STARTED event should have empty eventData';
-        }
-        break;
+        return this._validateStartedEvent(eventDetails.eventData);
 
       case AnalyticsEventName.PAYMENT_INITIATED:
-        if (
-          !eventDetails.eventData ||
-          typeof eventDetails.eventData.transactionId !== 'string'
-        ) {
-          return 'PAYMENT_INITIATED event requires transactionId as a string';
-        }
-        if (eventDetails.eventData.products) {
-          if (!Array.isArray(eventDetails.eventData.products)) {
-            return 'products must be an array';
-          }
-          for (const product of eventDetails.eventData.products) {
-            if (!product.id) {
-              return 'product id is required';
-            }
-            if (typeof product.id !== 'string') {
-              return 'product id must be a string';
-            }
-            if (product.quantity && typeof product.quantity !== 'number') {
-              return 'product quantity must be a number';
-            }
-          }
-        }
-        if (
-          eventDetails.eventData.amount &&
-          typeof eventDetails.eventData.amount !== 'number'
-        ) {
-          return 'amount must be a number';
-        }
-        if (
-          eventDetails.eventData.currency &&
-          typeof eventDetails.eventData.currency !== 'string'
-        ) {
-          return 'currency must be a string';
-        }
-        if (
-          eventDetails.eventData.promoCodes &&
-          (!Array.isArray(eventDetails.eventData.promoCodes) ||
-            !eventDetails.eventData.promoCodes.every(
-              (code) => typeof code === 'string'
-            ))
-        ) {
-          return 'promoCodes must be an array of strings';
-        }
-        break;
+        return this._validatePaymentInitiatedEvent(eventDetails.eventData);
 
       case AnalyticsEventName.ERROR_OCCURRED:
-        if (
-          !eventDetails.eventData ||
-          typeof eventDetails.eventData.errorCode !== 'string'
-        ) {
-          return 'ERROR_OCCURRED event requires errorCode as a string';
-        }
-        if (
-          eventDetails.eventData.errorMessage &&
-          typeof eventDetails.eventData.errorMessage !== 'string'
-        ) {
-          return 'errorMessage must be a string';
-        }
-        break;
+        return this._validateErrorOccurredEvent(eventDetails.eventData);
 
       case AnalyticsEventName.CUSTOM:
-        if (
-          !eventDetails.eventData ||
-          typeof eventDetails.eventData.customEventName !== 'string'
-        ) {
-          return 'CUSTOM event requires customEventName as a string in eventData';
+        return this._validateCustomEvent(eventDetails.eventData);
+    }
+
+    return null;
+  }
+
+  _validateStartedEvent(eventData) {
+    if (eventData !== null && eventData !== undefined) {
+      return `${AnalyticsEventName.STARTED} event should have empty eventData`;
+    }
+    return null;
+  }
+
+  _validatePaymentInitiatedEvent(eventData) {
+    if (!eventData) {
+      return `${AnalyticsEventName.PAYMENT_INITIATED} event requires eventData`;
+    }
+
+    const allowedPaymentFields = ['transactionId', 'products', 'amount', 'currency', 'promoCodes'];
+    const paymentFields = Object.keys(eventData);
+    const additionalPaymentFields = paymentFields.filter(field => !allowedPaymentFields.includes(field));
+    if (additionalPaymentFields.length > 0) {
+      return `${AnalyticsEventName.PAYMENT_INITIATED} event does not allow additional fields in eventData: ${additionalPaymentFields.join(', ')}`;
+    }
+
+    if (!eventData.transactionId) {
+      return `${AnalyticsEventName.PAYMENT_INITIATED} event requires transactionId in eventData to be defined`;
+    }
+    if (typeof eventData.transactionId !== 'string') {
+      return `${AnalyticsEventName.PAYMENT_INITIATED} event requires transactionId in eventData to be a string`;
+    }
+
+    if (eventData.products) {
+      if (!Array.isArray(eventData.products)) {
+        return `${AnalyticsEventName.PAYMENT_INITIATED} event requires products in eventData to be an array`;
+      }
+      for (const product of eventData.products) {
+        const allowedProductFields = ['id', 'quantity'];
+        const productFields = Object.keys(product);
+        const additionalProductFields = productFields.filter(field => !allowedProductFields.includes(field));
+        if (additionalProductFields.length > 0) {
+          return `${AnalyticsEventName.PAYMENT_INITIATED} event does not allow additional fields in eventData.products item: ${additionalProductFields.join(', ')}`;
         }
-        break;
+
+        if (!product.id) {
+          return `${AnalyticsEventName.PAYMENT_INITIATED} event requires id in eventData.products item to be defined`;
+        }
+        if (typeof product.id !== 'string') {
+          return `${AnalyticsEventName.PAYMENT_INITIATED} event requires id in eventData.products item to be a string`;
+        }
+        if (product.quantity && typeof product.quantity !== 'number') {
+          return `${AnalyticsEventName.PAYMENT_INITIATED} event requires quantity in eventData.products item to be a number`;
+        }
+      }
+    }
+
+    if (eventData.amount && typeof eventData.amount !== 'number') {
+      return `${AnalyticsEventName.PAYMENT_INITIATED} event requires amount in eventData to be a number`;
+    }
+    if (eventData.currency && typeof eventData.currency !== 'string') {
+      return `${AnalyticsEventName.PAYMENT_INITIATED} event requires currency in eventData to be a string`;
+    }
+    if (
+      eventData.promoCodes &&
+      (!Array.isArray(eventData.promoCodes) ||
+        !eventData.promoCodes.every((code) => typeof code === 'string'))
+    ) {
+      return `${AnalyticsEventName.PAYMENT_INITIATED} event requires promoCodes in eventData to be an array of strings`;
+    }
+
+    return null;
+  }
+
+  _validateErrorOccurredEvent(eventData) {
+    if (!eventData) {
+      return `${AnalyticsEventName.ERROR_OCCURRED} event requires eventData`;
+    }
+
+    const allowedErrorFields = ['errorCode', 'errorMessage'];
+    const errorFields = Object.keys(eventData);
+    const additionalErrorFields = errorFields.filter(field => !allowedErrorFields.includes(field));
+    if (additionalErrorFields.length > 0) {
+      return `${AnalyticsEventName.ERROR_OCCURRED} event does not allow additional fields in eventData: ${additionalErrorFields.join(', ')}`;
+    }
+
+    if (!eventData.errorCode) {
+      return `${AnalyticsEventName.ERROR_OCCURRED} event requires errorCode in eventData to be defined`;
+    }
+    if (typeof eventData.errorCode !== 'string') {
+      return `${AnalyticsEventName.ERROR_OCCURRED} event requires errorCode in eventData to be a string`;
+    }
+    if (eventData.errorMessage && typeof eventData.errorMessage !== 'string') {
+      return `${AnalyticsEventName.ERROR_OCCURRED} event requires errorMessage in eventData to be a string`;
+    }
+
+    return null;
+  }
+
+  _validateCustomEvent(eventData) {
+    if (!eventData) {
+      return `${AnalyticsEventName.CUSTOM} event requires eventData`;
+    }
+
+    const allowedCustomFields = ['customEventName', 'customEventData'];
+    const customFields = Object.keys(eventData);
+    const additionalCustomFields = customFields.filter(field => !allowedCustomFields.includes(field));
+    if (additionalCustomFields.length > 0) {
+      return `${AnalyticsEventName.CUSTOM} event does not allow additional fields in eventData: ${additionalCustomFields.join(', ')}`;
+    }
+
+    if (!eventData.customEventName) {
+      return `${AnalyticsEventName.CUSTOM} event requires customEventName in eventData to be defined`;
+    }
+    if (typeof eventData.customEventName !== 'string') {
+      return `${AnalyticsEventName.CUSTOM} event requires customEventName in eventData to be a string`;
     }
 
     return null;
