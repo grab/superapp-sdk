2c583d757537b7f7fab960680a9216f966136dd8
Maxime Debersaques <maxime.debersaques@grabtaxi.com>
2025-05-20 05:56:15 +0200

    [CXTTM-0000] PROCESSING

--- src/ContainerModule.js
+++ src/ContainerModule.js
@@ -24,7 +24,7 @@ export const PaymentStatusCode = {
   SUCCESS: 'SUCCESS',
   FAILURE: 'FAILURE',
   CANCELLED: 'CANCELLED',
-  UNKNOWN: 'UNKNOWN',
+  PROCESSING: 'PROCESSING',
 };
 
 export class ContainerModule {
